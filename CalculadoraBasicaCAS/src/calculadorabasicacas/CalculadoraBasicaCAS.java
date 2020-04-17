c7e88ce27d523071f4d1fac5b1da7468e54cd394
Marcelo <cheloz_20@hotmail.com>
2020-04-17 11:34:50 -0400

    Menú creado, se puede restar y dividir

--- CalculadoraBasicaCAS/src/calculadorabasicacas/CalculadoraBasicaCAS.java
+++ CalculadoraBasicaCAS/src/calculadorabasicacas/CalculadoraBasicaCAS.java
@@ -5,6 +5,8 @@
  */
 package calculadorabasicacas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Chelo
@@ -15,7 +17,69 @@ public class CalculadoraBasicaCAS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+       
+        mostrarMenu();
+             
+        
     }
+    
+    
+    public static int restar (int n1, int n2){
+        return  n1-n2;       
+    }
+    
+    public static int dividir (int n1, int n2){
+        float resultado = n1/n2;
+        int resultadoAInt = Math.round(resultado);
+        return resultadoAInt;
+    }
+    
+    
+    public static void mostrarMenu(){
+          
+        System.out.println("Eliga operación:");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        
+        Scanner s = new Scanner(System.in);
+        
+        int opcionElegida = s.nextInt();
+        
+        if(opcionElegida==1){
+            //Sumar
+            
+            
+        }else if (opcionElegida==2){
+            //Restar
+            System.out.print("Digite primer número:");
+            int n1 = s.nextInt();
+            System.out.print("Digite segundo número:");
+            int n2 = s.nextInt();
+            int resultado = restar(n1,n2);
+            System.out.println("El resultado es:"+resultado);
+            mostrarMenu();
+            
+            
+            
+        }
+        else if (opcionElegida==3){
+            //Multiplicar
+        }
+        else if (opcionElegida==4){
+            //Dividir
+            System.out.print("Digite primer número:");
+            int n1 = s.nextInt();
+            System.out.print("Digite segundo número:");
+            int n2 = s.nextInt();
+            int resultado = dividir(n1,n2);
+            System.out.println("El resultado es:"+resultado);
+            mostrarMenu();
+        }
+    }
+    
+    
     
 }
